4bfa9110a8258028e8d586e3cce5fe314b0a9ea7
Kris Albro <krs5a@yahoo.com>
2017-06-09 16:20:45 -0400

    changed it all

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-var formElement = document.querySelector('#form');
-console.log(document);
+let holder = "";
+function run (x){
+  holder = holder.concat(x.value);
+  setValue(holder)
+  console.log(holder);
+}
+function runClear(){
+  holder = "";
+  setValue(holder);
+  console.log(holder);
+}
+function runEqual(){
+  var total = eval(holder);
+  setValue(total)
+}
+ function setValue(setValue){
+   var newValue = document.getElementById('input-box');
+   newValue.value = setValue;
+ }
